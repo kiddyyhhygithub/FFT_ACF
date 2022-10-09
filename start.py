45044038f9ac5507ac83e140bb2a1649ebfe70e1
richard <11730301+richardyh@user.noreply.gitee.com>
2022-10-09 14:06:20 +0800

    Initial commit

---
+++ start.py
@@ -0,0 +1,19 @@
+# _*_ coding:utf-8 _*_
+# Author:颜华
+# Date&Time: 13:05
+# CurrentFile:start.py
+# EMAIL:kiddyflash@163.com
+import matplotlib.pylab as plt
+import numpy as np
+
+def train():
+
+
+
+
+
+    return  None
+
+
+if __name__ == '__main__':
+    train()
